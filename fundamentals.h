a6a9088ba25a5ccb76edf07989ab5e0f4bfdca5b
adityam2905 <am24csb1a03@student.nitw.ac.in>
2025-02-26 10:47:17 +0530

    Update fundamentals.h

--- fundamentals.h
+++ fundamentals.h
@@ -1,1 +1,16 @@
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+#include <bits/stdc++.h>
+#include "node.h"
+using namespace std;
+
+void Updateheight(Node* root);
+Node* LLR(Node* root);
+Node* RRR(Node* root);
+Node* LRR(Node* root);
+Node* RLR(Node* root);
+Node* search(Node* root, int key);
+
+#endif // AVLTREE_H
 
