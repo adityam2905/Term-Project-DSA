9135589333cc454f7d43980f573c91d6740c79e0
adityam2905 <am24csb1a03@student.nitw.ac.in>
2025-03-25 20:40:58 +0530

    Update filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -1,6 +1,7 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -8,16 +9,21 @@
 #include <string>
 #include "avl.h"  // Assuming AVLTree.h defines the AVL tree and Node structure
 
+
 using namespace std;
+
 
 // Utility function to split a string by commas
 vector<string> split_by_comma(const string& line);
 
+
 // Function to process the file and populate the AVL tree
 void process_file(const char* filename, AVL& tree);
 
+
 // Function to handle initial database of students and courses
 void handle_initial_db(AVL& tree, vector<string> lines, vector<Course*> student_courses);
+
 
 // Functions for different operations on the AVL tree
 void handle_roll_greater_than(AVL &tree, string line);
@@ -31,6 +37,7 @@ void handle_delete_student(AVL &tree, string line);
 void handle_add_course(AVL &tree, string line);
 void handle_modify_course(AVL& tree, string line);
 void handle_delete_course(AVL &tree, string line);
+
 
 // Functions to print student data based on specific conditions
 void printGreaterThan(Node* root, int roll_no);
